e4e5071c3a9905871fb016f98ba13801701679f8
jonti <jonti.savani@vphrase.com>
2020-04-19 15:53:27 +0530

    Updates

---
+++ precache-manifest.d6abb03c211daf9a5c4b7a819e3f59ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f327c8562d147cddfaa5585b20cc389",
+    "url": "/quotesify/index.html"
+  },
+  {
+    "revision": "230c1345e92d180928b9",
+    "url": "/quotesify/static/css/main.e8ee2e90.chunk.css"
+  },
+  {
+    "revision": "b6fa48877824035501ac",
+    "url": "/quotesify/static/js/2.76c6f207.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotesify/static/js/2.76c6f207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230c1345e92d180928b9",
+    "url": "/quotesify/static/js/main.95e713ea.chunk.js"
+  },
+  {
+    "revision": "29f2b8979d9245475eb8",
+    "url": "/quotesify/static/js/runtime-main.de63cc2d.js"
+  },
+  {
+    "revision": "911e46233713364def1083764ec536a5",
+    "url": "/quotesify/static/media/background.911e4623.jpg"
+  }
+]);
